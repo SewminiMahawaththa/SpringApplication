4e5bacbd82dc1455816fd28f337e2b4e3450d3af
it21308598 <it21308598@my.sliit.lk>
2024-04-12 14:30:35 +0530

    Initial Commit

---
+++ src/test/java/com/example/SpringUserApplication/SpringUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringUserApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
